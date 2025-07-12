1508d549749e6c366d59dfadc740baa2c00811ca
taran190 <tedabc52@gmail.com>
2025-07-12 15:28:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,21 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "bad_example" {
-  bucket = "my-insecure-bucket"
-  acl    = "public-read" # tfsec will warn about this
-}
+resource "aws_security_group" "example" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.1.0/24"]
+  }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
